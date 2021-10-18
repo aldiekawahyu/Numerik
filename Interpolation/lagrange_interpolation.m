b2688f0ed62be59414f9bb17b87a70eeb91287f8
Aldi E. W. Widianto <66999159+aldiekawahyu@users.noreply.github.com>
2021-10-18 16:19:48 +0700

    Add coefficients of lagrangian function

--- Interpolation/lagrange_interpolation.m
+++ Interpolation/lagrange_interpolation.m
@@ -1,19 +1,24 @@
-function [poly,y_interp] = lagrange_interpolation(x,y,n,x_interp)
+function [poly, coefs, y_interp] = lagrange_interpolation(x,y,n,x_interp)
     if nargin == 3
         x_interp = [];
     end
     xs = x; ys = y;
     poly = 0;
+    coefs = zeros(length(xs),1);
+    coef = 1;
     for i = 1:n+1
-        product = ys(i);
-        syms x;
+        product = ys(i);        
+        syms x;       
         for j = 1:n+1
             if i ~= j
                 product = product*(x-xs(j))/(xs(i)-xs(j));
+                coef = coef*(xs(i)-xs(j));
             end
-        end
+        end        
+        coefs(i) = ys(i)/coef;
+        coef = 1;
         poly = poly+product;
     end
     x = x_interp;
-    y_interp = double(subs(y));
+    y_interp = double(subs(poly));
 end
